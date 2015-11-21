8ccd4bb0e0f90cf1702227b2be772933d65ef7ad
Justin Leo Ye <justinleoye@gmail.com>
2015-11-21 19:10:33 +0800

    fix

--- o2o_info_collection/pipelines.py
+++ o2o_info_collection/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
 
 class O2OInfoCollectionPipeline(object):
     def __init__(self, mongo_uri, mongo_db):
